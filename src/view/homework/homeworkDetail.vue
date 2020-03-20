0ff7116d00a56f2823e4fbbe9442a5f70ded7da0
bobo <bobo@qq.com>
2020-03-20 21:58:45 +0800

    更新页面

--- src/view/homework/homeworkDetail.vue
+++ src/view/homework/homeworkDetail.vue
@@ -2,7 +2,6 @@
   <div class="homeworkDetail">
     <h2 style="color:#2d8cf0;margin:10px 0;text-align:center;">马克思作业五</h2>
     <div class="homework-content">
-      <Button class="exit-btn" type="default"><Icon type="md-power" style="margin-right:3px;" />退出作答</Button>
       <h3>一、<Icon type="ios-hand" color="#ff652f" size="20" style="margin-right:3px;" />单选题（50分）</h3>
       <div class="choice">
         <p class="choice-question">1、根据IID模型，开发过程中如果发现不能按期完成任务时应该（10分）</p>
@@ -12,6 +11,7 @@
           <Radio label="C" class="radio">C.取消或简化某些任务</Radio>
           <Radio label="D" class="radio">D.早期计划不周，发现问题时为期已晚，项目延期不可避免</Radio>
         </RadioGroup>
+        <p><b style="color:#2d8cf0;">答案：</b>A</p>
         <p class="choice-question">2、根据IID模型，开发过程中如果发现不能按期完成任务时应该（10分）</p>
         <RadioGroup class="option" v-model="userAnswer[1]">
           <Radio label="A" class="radio">A.增加开发人员</Radio>
@@ -19,6 +19,7 @@
           <Radio label="C" class="radio">C.取消或简化某些任务</Radio>
           <Radio label="D" class="radio">D.早期计划不周，发现问题时为期已晚，项目延期不可避免</Radio>
         </RadioGroup>
+        <p><b style="color:#2d8cf0;">答案：</b>A</p>
         <p class="choice-question">3、根据IID模型，开发过程中如果发现不能按期完成任务时应该（10分）</p>
         <RadioGroup class="option" v-model="userAnswer[2]">
           <Radio label="A" class="radio">A.增加开发人员</Radio>
@@ -26,6 +27,7 @@
           <Radio label="C" class="radio">C.取消或简化某些任务</Radio>
           <Radio label="D" class="radio">D.早期计划不周，发现问题时为期已晚，项目延期不可避免</Radio>
         </RadioGroup>
+        <p><b style="color:#2d8cf0;">答案：</b>A</p>
         <p class="choice-question">4、根据IID模型，开发过程中如果发现不能按期完成任务时应该（10分）</p>
         <RadioGroup class="option" v-model="userAnswer[3]">
           <Radio label="A" class="radio">A.增加开发人员</Radio>
@@ -33,6 +35,7 @@
           <Radio label="C" class="radio">C.取消或简化某些任务</Radio>
           <Radio label="D" class="radio">D.早期计划不周，发现问题时为期已晚，项目延期不可避免</Radio>
         </RadioGroup>
+        <p><b style="color:#2d8cf0;">答案：</b>A</p>
         <p class="choice-question">5、根据IID模型，开发过程中如果发现不能按期完成任务时应该（10分）</p>
         <RadioGroup class="option" v-model="userAnswer[4]">
           <Radio label="A" class="radio">A.增加开发人员</Radio>
@@ -40,6 +43,7 @@
           <Radio label="C" class="radio">C.取消或简化某些任务</Radio>
           <Radio label="D" class="radio">D.早期计划不周，发现问题时为期已晚，项目延期不可避免</Radio>
         </RadioGroup>
+        <p><b style="color:#2d8cf0;">答案：</b>A</p>
       </div>
       <h3>二、<Icon type="md-checkbox-outline" color="green" size="20" style="margin-right:3px;" />多选题（30分）</h3>
       <div class="choice">
@@ -50,6 +54,7 @@
           <Checkbox label="C" class="radio">C.取消或简化某些任务</Checkbox>
           <Checkbox label="D" class="radio">D.早期计划不周，发现问题时为期已晚，项目延期不可避免</Checkbox>
         </CheckboxGroup>
+        <p><b style="color:#2d8cf0;">答案：</b>[A, B]</p>
         <p class="choice-question">2、根据IID模型，开发过程中如果发现不能按期完成任务时应该（10分）</p>
         <CheckboxGroup class="option" v-model="userAnswer2[1]">
           <Checkbox label="A" class="radio">A.增加开发人员</Checkbox>
@@ -57,6 +62,7 @@
           <Checkbox label="C" class="radio">C.取消或简化某些任务</Checkbox>
           <Checkbox label="D" class="radio">D.早期计划不周，发现问题时为期已晚，项目延期不可避免</Checkbox>
         </CheckboxGroup>
+        <p><b style="color:#2d8cf0;">答案：</b>[A, B]</p>
         <p class="choice-question">3、根据IID模型，开发过程中如果发现不能按期完成任务时应该（10分）</p>
         <CheckboxGroup class="option" v-model="userAnswer2[2]">
           <Checkbox label="A" class="radio">A.增加开发人员</Checkbox>
@@ -64,6 +70,7 @@
           <Checkbox label="C" class="radio">C.取消或简化某些任务</Checkbox>
           <Checkbox label="D" class="radio">D.早期计划不周，发现问题时为期已晚，项目延期不可避免</Checkbox>
         </CheckboxGroup>
+        <p><b style="color:#2d8cf0;">答案：</b>[A, B]</p>
         <p class="choice-question">4、根据IID模型，开发过程中如果发现不能按期完成任务时应该（10分）</p>
         <CheckboxGroup class="option" v-model="userAnswer2[3]">
           <Checkbox label="A" class="radio">A.增加开发人员</Checkbox>
@@ -71,6 +78,7 @@
           <Checkbox label="C" class="radio">C.取消或简化某些任务</Checkbox>
           <Checkbox label="D" class="radio">D.早期计划不周，发现问题时为期已晚，项目延期不可避免</Checkbox>
         </CheckboxGroup>
+        <p><b style="color:#2d8cf0;">答案：</b>[A, B]</p>
         <p class="choice-question">5、根据IID模型，开发过程中如果发现不能按期完成任务时应该（10分）</p>
         <CheckboxGroup class="option" v-model="userAnswer2[4]">
           <Checkbox label="A" class="radio">A.增加开发人员</Checkbox>
@@ -78,16 +86,18 @@
           <Checkbox label="C" class="radio">C.取消或简化某些任务</Checkbox>
           <Checkbox label="D" class="radio">D.早期计划不周，发现问题时为期已晚，项目延期不可避免</Checkbox>
         </CheckboxGroup>
+        <p><b style="color:#2d8cf0;">答案：</b>[A, B]</p>
       </div>
       <div class="shortAnswer">
         <h3>三、<Icon type="md-pricetags" color="blue" size="20" style="margin-right:3px;" />简答题（20分）</h3>
         <p class="shortAnswer-question">1、假定要对某个方法bool triangle(int a, int b, int c), 输入三个整形参数作为三个边长，判断他们能否构成一个三角形，输出为布尔型true 或 false, 请设计测试用例。（10分）</p>
         <Input class="shortAnswer-ans" type="textarea" :rows="4" placeholder="请在框内输入合适的答案……" />
+        <p style="margin:10px 0;">参考答案：</p>
+        <Input v-model="shortAnswer" type="textarea" placeholder="Enter something..." />
         <p class="shortAnswer-question">2、假定要对某个方法bool triangle(int a, int b, int c), 输入三个整形参数作为三个边长，判断他们能否构成一个三角形，输出为布尔型true 或 false, 请设计测试用例。（10分）</p>
         <Input class="shortAnswer-ans" type="textarea" :rows="4" placeholder="请在框内输入合适的答案……" />
-      </div>
-      <div class="submit-btn">
-        <Button type="default" style="color:#fff;background-color:green;">提交作业</Button>
+        <p style="margin:10px 0;">参考答案：</p>
+        <Input v-model="shortAnswer" type="textarea" placeholder="Enter something..." />
       </div>
     </div>
   </div>
@@ -103,6 +113,7 @@ export default {
     return {
       userAnswer: [1, 2, 3, 4, 5],
       userAnswer2: [1, 2, 3, 4, 5],
+      shortAnswer: '社会主义（socialism）是一种社会学思想，诞生于16世纪初，主张整个社会应作为整体，由社会拥有和控制产品、资本、土地、资产等，其管理和分配基于公众利益。19世纪30至40年代，“社会主义”的概念在西欧广为流传，发展出不同分支。马克思和恩格斯对社会主义发展出了他们的理论体系，亦认为社会主义社会是资本主义社会向共产主义社会过渡的社会形态。',
       homeworkData: [
         {
           title: '马克思作业五',
@@ -181,6 +192,7 @@ export default {
   }
   .shortAnswer {
     width: 100%;
+    margin-top: 10px;
     .shortAnswer-question {
       margin-top: 10px;
     }
--- src/view/homework/homeworkStatis.vue
+++ src/view/homework/homeworkStatis.vue
@@ -1,22 +1,81 @@
 <template>
-  <div class="homework">
+  <div class="homeworkStatis">
     <h1 style="color:#2d8cf0;margin:10px 0;">马克思作业5</h1>
-    <Collapse v-model="value2">
+    <Collapse v-model="value2" accordion>
       <Panel name="1">
         成绩统计
         <div slot="content" class="score">
           <p style="margin-bottom:10px;">A:（≥90）&nbsp;&nbsp;B:（70~89）&nbsp;&nbsp;C:（60~79）&nbsp;&nbsp;D:（小于60）</p>
-          <div style="width:500px;height:300px;" class="score-statis" ref="barScoreStatis"></div>
-          <div style="width:500px;height:300px;" class="score-statis" ref="pieScoreStatis"></div>
+          <div style="width:100%;height:300px;">
+            <div style="width:500px;height:300px;" class="score-statis" ref="barScoreStatis"></div>
+            <div style="width:500px;height:300px;" class="score-statis" ref="pieScoreStatis"></div>
+          </div>
+          <Divider />
+          <div class="rank-list">
+            <h3 style="color:#2d8cf0;font-weight:bold;margin-bottom:10px;">成绩排行</h3>
+            <div style="width:100%;height:340px;display:flex;align-item:middle;">
+              <div>
+                <Table :columns="rankColumns" :data="rankList" :width="300" :height="340" border class="rankList"></Table>
+              </div>
+              <div style="margin-left:60px;">
+                <h3 style="margin-bottom:15px;"><span style="color:green">最高分：</span>蔡敏波（95分）</h3>
+                <h3 style="margin-bottom:15px;"><span style="color:red;">最低分：</span>蔡敏波（55分）</h3>
+                <h3 style="margin-bottom:15px;"><span style="color:#2d8cf0">平均分：</span>83分</h3>
+              </div>
+            </div>
+          </div>
         </div>
       </Panel>
       <Panel name="2">
         完成情况统计
-        <div slot="content">斯蒂夫·盖瑞·沃兹尼亚克（Stephen Gary Wozniak），美国电脑工程师，曾与史蒂夫·乔布斯合伙创立苹果电脑（今之苹果公司）。斯蒂夫·盖瑞·沃兹尼亚克曾就读于美国科罗拉多大学，后转学入美国著名高等学府加州大学伯克利分校（UC Berkeley）并获得电机工程及计算机（EECS）本科学位（1987年）。</div>
+        <div slot="content" class="completion">
+          <div class="completionData">
+            <Table border :columns="completionColumns" :data="completionData" :width="380"></Table>
+            <div class="list">
+              <Table class="list-table" border :columns="incompleteListColumns" :data="incompleteList" :width="150" :height="200"></Table>
+              <Table class="list-table" border :columns="completeListColumns" :data="completeList" :width="150" :max-height="200"></Table>
+            </div>
+          </div>
+          <div class="completionCharts">
+            <div style="width:300px;height:300px;" class="barCharts" ref="barCompleteCharts"></div>
+            <div style="width:300px;height:300px;" class="pieCharts" ref="pieCompleteCharts"></div>
+          </div>
+        </div>
       </Panel>
       <Panel name="3">
-        乔纳森·伊夫
-        <p slot="content">乔纳森·伊夫是一位工业设计师，现任Apple公司设计师兼资深副总裁，英国爵士。他曾参与设计了iPod，iMac，iPhone，iPad等众多苹果产品。除了乔布斯，他是对苹果那些著名的产品最有影响力的人。</p>
+        各题答题情况
+        <div slot="content" class="accuracy">
+          <p>各题答题正确率如下：</p>
+          <div>
+            <div style="float:left;width:500px;height:300px;" class="barCharts" ref="pieAccuracyCharts"></div>
+            <Table class="list-table" border :columns="accuracyColumns" :data="accuracyList" :width="300" :height="300"></Table>
+          </div>
+          <Divider />
+          <div class="select-nav">
+            <h3 style="color:#2d8cf0;font-weight:bold;margin-bottom:10px;">各题具体详细情况</h3>
+            <div>
+              <b>请选择题数：</b>
+              <Select v-model="selectedNo" style="width:100px;margin:0 10px;" placeholder="请选择">
+                <Option v-for="item in selectNo" :key="item.value" :value="item.value">{{item.label}}</Option>
+              </Select>
+              <Button type="primary" style="margin-right:20px;">查询</Button>
+              <b>题型：</b>选择题
+            </div>
+            <div class="data">
+              <div class="accuracyData">
+                <Table border :columns="singleAccColumns" :data="singleAccData" :width="380"></Table>
+                <div class="list">
+                  <Table class="list-table" border :columns="correctListColumns" :data="correctList" :width="150" :height="200"></Table>
+                  <Table class="list-table" border :columns="incorrectListColumns" :data="incorrectList" :width="150" :max-height="200"></Table>
+                </div>
+              </div>
+              <div class="accuracyCharts">
+                <div style="width:300px;height:300px;float:left;" class="barCharts" ref="singleBarCharts"></div>
+                <div style="width:300px;height:300px;float:left;" class="pieCharts" ref="singlePieACharts"></div>
+              </div>
+            </div>
+          </div>
+        </div>
       </Panel>
     </Collapse>
   </div>
@@ -27,14 +86,249 @@ export default {
   name: 'homework',
   data () {
     return {
-      // 修改弹窗弹出判定
-      value2: '1'
+      value2: '1',
+      selectedNo: 1,
+      selectNo: [
+        {
+          value: '1',
+          label: '1'
+        },
+        {
+          value: '2',
+          label: '2'
+        },
+        {
+          value: '3',
+          label: '3'
+        },
+        {
+          value: '4',
+          label: '4'
+        },
+        {
+          value: '5',
+          label: '5'
+        }
+      ],
+      completionColumns: [
+        {
+          title: '完成作业人数',
+          key: 'completionNum',
+          align: 'center'
+        },
+        {
+          title: '未完成作业人数',
+          key: 'incompletionNum',
+          align: 'center'
+        },
+        {
+          title: '完成率',
+          key: 'completion',
+          align: 'center'
+        }
+      ],
+      completionData: [
+        {
+          completionNum: 70,
+          incompletionNum: 5,
+          completion: '93%'
+        }
+      ],
+      incompleteListColumns: [
+        {
+          title: '未完成作业名单',
+          key: 'name',
+          align: 'center'
+        }
+      ],
+      completeListColumns: [
+        {
+          title: '完成作业名单',
+          key: 'name',
+          align: 'center'
+        }
+      ],
+      incompleteList: [
+        {
+          name: '蔡敏波'
+        },
+        {
+          name: '蔡敏波'
+        },
+        {
+          name: '蔡敏波'
+        },
+        {
+          name: '蔡敏波'
+        }
+      ],
+      completeList: [
+        {
+          name: '王小明'
+        },
+        {
+          name: '王小明'
+        },
+        {
+          name: '王小明'
+        }
+      ],
+      accuracyColumns: [
+        {
+          title: '题目',
+          key: 'title',
+          align: 'center'
+        },
+        {
+          title: '正确率',
+          key: 'accuracy',
+          align: 'center'
+        }
+      ],
+      accuracyList: [
+        {
+          title: 1,
+          accuracy: '70%'
+        },
+        {
+          title: 1,
+          accuracy: '70%'
+        },
+        {
+          title: 1,
+          accuracy: '70%'
+        },
+        {
+          title: 1,
+          accuracy: '70%'
+        },
+        {
+          title: 1,
+          accuracy: '70%'
+        },
+        {
+          title: 1,
+          accuracy: '70%'
+        },
+        {
+          title: 1,
+          accuracy: '70%'
+        }
+      ],
+      singleAccColumns: [
+        {
+          title: '回答正确人数',
+          key: 'correctnNum',
+          align: 'center'
+        },
+        {
+          title: '回答错误人数',
+          key: 'incorrectNum',
+          align: 'center'
+        },
+        {
+          title: '正确率',
+          key: 'accuracy',
+          align: 'center'
+        }
+      ],
+      singleAccData: [
+        {
+          correctnNum: 70,
+          incorrectNum: 5,
+          accuracy: '93%'
+        }
+      ],
+      correctListColumns: [
+        {
+          title: '回答正确学生名单',
+          key: 'name',
+          align: 'center'
+        }
+      ],
+      incorrectListColumns: [
+        {
+          title: '回答错误同学名单',
+          key: 'name',
+          align: 'center'
+        }
+      ],
+      correctList: [
+        {
+          name: '蔡敏波'
+        },
+        {
+          name: '蔡敏波'
+        },
+        {
+          name: '蔡敏波'
+        },
+        {
+          name: '蔡敏波'
+        }
+      ],
+      incorrectList: [
+        {
+          name: '王小明'
+        },
+        {
+          name: '王小明'
+        },
+        {
+          name: '王小明'
+        }
+      ],
+      rankColumns: [
+        {
+          title: '名次',
+          key: 'no',
+          align: 'center'
+        },
+        {
+          title: '姓名',
+          key: 'name',
+          align: 'center'
+        }
+      ],
+      rankList: [
+        {
+          no: 1,
+          name: '蔡敏波'
+        },
+        {
+          no: 1,
+          name: '蔡敏波'
+        },
+        {
+          no: 1,
+          name: '蔡敏波'
+        },
+        {
+          no: 1,
+          name: '蔡敏波'
+        },
+        {
+          no: 1,
+          name: '蔡敏波'
+        },
+        {
+          no: 1,
+          name: '蔡敏波'
+        },
+        {
+          no: 1,
+          name: '蔡敏波'
+        }
+      ]
     }
   },
   mounted () {
     // this.initScoreStatis()
     this.$nextTick(() => {
       this.initScoreStatis()
+      this.initCompletionStatis()
+      this.initAccuracyBar()
+      this.initSingleAccuracy()
     })
   },
   methods: {
@@ -50,7 +344,8 @@ export default {
           data: ['成绩A', '成绩B', '成绩C', '成绩D']
         },
         yAxis: {
-          type: 'value'
+          type: 'value',
+          name: '人数（人）'
         },
         series: [{
           data: [120, 200, 150, 80],
@@ -66,6 +361,11 @@ export default {
       pieScoreStatis.style.width = '500px'
       let pieChart = this.$echarts.init(pieScoreStatis)
       pieChart.setOption({
+        legend: {
+          orient: 'vertical',
+          left: 10,
+          data: ['成绩A', '成绩B', '成绩C', '成绩D']
+        },
         series: [
           {
             type: 'pie',
@@ -79,27 +379,208 @@ export default {
           }
         ]
       })
+    },
+    initCompletionStatis () {
+      let barChart = this.$refs.barCompleteCharts
+      barChart.style.width = '300px'
+      let completionBar = this.$echarts.init(barChart)
+      completionBar.setOption({
+        color: ['#2d8cf0'],
+        xAxis: {
+          type: 'category',
+          data: ['完成作业人数', '未完成作业人数']
+        },
+        yAxis: {
+          type: 'value',
+          name: '人数（人）'
+        },
+        series: [{
+          data: [70, 5],
+          type: 'bar'
+        }]
+      })
+      completionBar.resize()
+      window.addEventListener('resize', function () {
+        completionBar.resize()
+      })
+      let pieChart = this.$refs.pieCompleteCharts
+      pieChart.style.width = '300px'
+      let completionPie = this.$echarts.init(pieChart)
+      completionPie.setOption({
+        legend: {
+          left: 10,
+          data: ['完成作业', '未完成作业']
+        },
+        series: [
+          {
+            type: 'pie',
+            radius: '55%',
+            data: [
+              { value: 70, name: '完成作业' },
+              { value: 5, name: '未完成作业' }
+            ]
+          }
+        ]
+      })
+    },
+    initAccuracyBar () {
+      let barChart = this.$refs.pieAccuracyCharts
+      barChart.style.width = '500px'
+      let accuracyBar = this.$echarts.init(barChart)
+      accuracyBar.setOption({
+        color: ['#2d8cf0'],
+        xAxis: {
+          type: 'category',
+          data: ['1', '2', '3', '4', '5', '6', '7', '8']
+        },
+        yAxis: {
+          type: 'value',
+          name: '正确率（%）'
+        },
+        series: [{
+          data: [70, 50, 60, 90, 80, 70, 40, 100],
+          type: 'bar'
+        }]
+      })
+      accuracyBar.resize()
+      window.addEventListener('resize', function () {
+        accuracyBar.resize()
+      })
+    },
+    initSingleAccuracy () {
+      let barChart = this.$refs.singleBarCharts
+      barChart.style.width = '300px'
+      let singleBar = this.$echarts.init(barChart)
+      singleBar.setOption({
+        color: ['#2d8cf0'],
+        xAxis: {
+          type: 'category',
+          data: ['回答正确人数', '回答错误人数']
+        },
+        yAxis: {
+          type: 'value',
+          name: '人数（人）'
+        },
+        series: [{
+          data: [70, 5],
+          type: 'bar'
+        }]
+      })
+      singleBar.resize()
+      window.addEventListener('resize', function () {
+        singleBar.resize()
+      })
+      let pieChart = this.$refs.singlePieACharts
+      pieChart.style.width = '300px'
+      let singlePie = this.$echarts.init(pieChart)
+      singlePie.setOption({
+        legend: {
+          left: 10,
+          data: ['回答正确人数', '回答错误人数']
+        },
+        series: [
+          {
+            type: 'pie',
+            radius: '55%',
+            data: [
+              { value: 70, name: '回答正确人数' },
+              { value: 5, name: '回答错误人数' }
+            ]
+          }
+        ]
+      })
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
-.homework {
-  height: 700px;
+.homeworkStatis {
+  height: 600px;
+  min-height: 600px;
   h1 {
     border-bottom: 1px solid rgba(0, 0, 0, 0.5);
     padding-bottom: 8px;
   }
   .score {
-    height: 360px;
+    height: 750px;
     width: 100%;
     .score-statis {
       border: 1px solid rgba(0, 0, 0, 0.5);
       float: left;
       margin-right:20px;
     }
+    .rank-list {
+      display: block;
+      width: 100%;
+      height: 350px;
+    }
   }
-
+  .completion {
+    height: 350px;
+    width: 100%;
+    .completionData {
+      float: left;
+      width: 40%;
+      .list {
+        margin-top: 20px;
+        .list-table {
+          float: left;
+          margin-right: 30px;
+        }
+      }
+    }
+    .completionCharts {
+      float: left;
+      width: 60%;
+      .barCharts {
+        float: left;
+        border: 1px solid rgba(0, 0, 0, 0.5);
+      }
+      .pieCharts {
+        float: left;
+        margin-left: 20px;
+        border: 1px solid rgba(0, 0, 0, 0.5);
+      }
+    }
+  }
+  .accuracy {
+    width: 100%;
+    height: 800px;
+    min-height: 800px;
+    .data {
+      margin-top: 20px;
+      height: 600px;
+      width: 100%;
+      display: flex;
+      align-items: middle;
+      .accuracyData {
+        width: 40%;
+        height: 500px;
+        float: left;
+        .list {
+          margin-top: 20px;
+          .list-table {
+            float: left;
+            margin-right: 30px;
+          }
+        }
+      }
+      .accuracyCharts {
+        width: 60%;
+        height: 500px;
+        float: left;
+        .barCharts {
+          float: left;
+          border: 1px solid rgba(0, 0, 0, 0.5);
+        }
+        .pieCharts {
+          float: left;
+          margin-left: 20px;
+          border: 1px solid rgba(0, 0, 0, 0.5);
+        }
+      }
+    }
+  }
 }
 </style>
